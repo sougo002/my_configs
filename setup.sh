d630929bf12c0b490251088d6eebba5f40abe4a4
sougo <sougo_002@outlook.jp>
2023-10-02 14:41:21 +0900

    bash用rcファイル追加

--- setup.sh
+++ setup.sh
@@ -9,10 +9,18 @@ if [ ! -f ~/.my_zshenv ]; then
   ln .my_zshenv ~/.my_zshenv
 fi
 
+if [ ! -f ~/.my_bashrc ]; then
+  ln .my_bashrc ~/.my_bashrc
+fi
+
 if [ -f ~/.zshrc ] && [ `grep ". ~/.my_zshrc" ~/.zshrc | wc -l` == "0" ]; then
   echo ". ~/.my_zshrc" >> ~/.zshrc
 fi
 
 if [ -f ~/.zshenv ] && [ `grep ". ~/.my_zshenv" ~/.zshenv | wc -l` == "0" ]; then
   echo ". ~/.my_zshenv" >> ~/.zshenv
+fi
+
+if [ -f ~/.bashrc ] && [ `grep ". ~/.my_bashrc" ~/.bashrc | wc -l` == "0" ]; then
+  echo ". ~/.my_bashrc" >> ~/.bashrc
 fi
