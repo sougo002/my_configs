54c6168fd1977c5698fde6645ecbb81ae32a0904
sougo <sougo_002@outlook.jp>
2025-05-31 12:03:24 +0900

    zsh設定追加

--- setup.sh
+++ setup.sh
@@ -3,15 +3,32 @@
 # git prompt
 if [ ! -d ~/.zsh ]; then
   mkdir ~/.zsh
-fi
-if [ ! -f ~/.zsh/git-prompt.sh ]; then
-  curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-fi
-if [ ! -f ~/.zsh/git-completion.bash ]; then
-  curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-fi
-if [ ! -f ~/.zsh/_git ]; then
-  curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+  cd ~/.zsh
+  if [ ! -f ~/.zsh/git-prompt.sh ]; then
+    curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+  fi
+  if [ ! -f ~/.zsh/git-completion.bash ]; then
+    curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  fi
+  if [ ! -f ~/.zsh/_git ]; then
+    curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+  fi
+  if ! grep -q "fpath=(~/.zsh \$fpath)" ~/.zshrc; then
+    cat >> ~/.zshrc << 'EOF'
+# Git settings
+fpath=(~/.zsh $fpath)
+source ~/.zsh/git-prompt.sh
+zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
+
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+EOF
+    echo ".zshrcにGit設定を追加しました"
+  fi
+  
+  cd ~
 fi
 
 # Install tools managed by homebrew
